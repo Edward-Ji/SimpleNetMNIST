bc276554cf1e4469d09223720be8b1c6fafdc69b
Edward Ji <jiziao6@gmail.com>
2024-07-01 17:32:59 +1000

    Bump gradio major version

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from PIL import Image
+
 import gradio as gr
 import torch
-
-from torchvision.transforms.functional import to_tensor
+from torchvision.transforms.functional import invert, resize, to_tensor
 
 from simpnet import SimpnetSlim310K
+
+CANVAS_SIZE = (256, 256)
 
 CLASSES = (
     '0 - zero',
@@ -23,24 +26,35 @@ MODEL_PATH = "models/simpnet_slim_310k.pt"
 simpnet_slim = SimpnetSlim310K(1, len(CLASSES))
 simpnet_slim.load_state_dict(torch.load(MODEL_PATH, map_location="cpu"))
 
-def predict(image):
-    if image is None:
-        return None
+def clear():
+    return Image.new(mode="L", size=CANVAS_SIZE, color="white")
+
+def predict(sketch):
+    image = sketch["composite"]
 
     simpnet_slim.eval()
     with torch.inference_mode():
-        x = to_tensor(image).unsqueeze(0)
-        logits = simpnet_slim(x)
+        x = invert(resize(to_tensor(image), [32, 32]))
+        logits = simpnet_slim(x.unsqueeze(0))
         probs = torch.softmax(logits, dim=1)
 
     return dict(zip(CLASSES, map(torch.Tensor.item, probs.squeeze())))
 
-app = gr.Interface(
-    fn=predict,
-    inputs=gr.Sketchpad(shape=(32, 32), image_mode="L", type="pil"),
-    outputs=gr.Label(),
-    live=True
-)
+with gr.Blocks() as app:
+    with gr.Row():
+        with gr.Column():
+            sketchpad = gr.Sketchpad(
+                value=clear(),
+                image_mode="L",
+                type="pil",
+                transforms=(),
+                layers=False,
+                canvas_size=CANVAS_SIZE,
+                )
+        with gr.Column():
+            label = gr.Label()
+    sketchpad.clear(clear, outputs=sketchpad)
+    sketchpad.change(predict, outputs=label, inputs=sketchpad)
 
 if __name__ == "__main__":
     app.launch()
